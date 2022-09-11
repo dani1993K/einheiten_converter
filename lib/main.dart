8cc73d47ca4b9fc1de1884ba77b2d3c706d58812
Daniel <lingling1993@web.de>
2022-09-11 14:29:48 +0200

    v1.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 final items = ['Meter', 'Kilometer','Feet','Mile'];
+//list of items for DropdownFields
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
   // This method runs the App.
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Einheiten Converter App',
-      home: HomeApp('Einheiten Converter'),
+    return const MaterialApp(
+      title: 'Einheiten Konverter App',
+      home: HomeApp('Einheiten Konverter'),
       //
     );
   }
 }
 
 class HomeApp extends StatelessWidget {
-  HomeApp(this.title);
+  const HomeApp(this.title, {super.key});
   final String title;
   @override
   Widget build(BuildContext context) {
@@ -27,31 +30,48 @@ class HomeApp extends StatelessWidget {
         title: Text(title),
       ),
       body: Column(
-        children: <Widget>[
+        children: const <Widget>[
           Padding(
-            padding: EdgeInsets.fromLTRB (0,20,0,5),
-            child: const Text('Wert', style: const TextStyle(fontSize: 25)),
+            padding: EdgeInsets.fromLTRB (0,20,0,0),
+            child: Text('Wert', style: TextStyle(fontSize: 25)),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB (20,5,20,5),
-            child: InputWert(),
+            padding: EdgeInsets.fromLTRB (20,0,20,5),
+            child: InputValue(),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB (0,15,0,5),
-            child: const Text('Von', style: const TextStyle(fontSize: 25)),
+            padding: EdgeInsets.fromLTRB (0,15,0,0),
+            child: Text('Von', style: TextStyle(fontSize: 25)),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB (20,5,20,5),
-            child: DropDownButtonVon(),
+            padding: EdgeInsets.fromLTRB (20,0,20,5),
+            child: DropDownButtonFrom(),
           ),
-          Text('Nach', style: const TextStyle(fontSize: 25)),
+          Padding(
+            padding: EdgeInsets.fromLTRB (0,15,0,0),
+            child: Text('Nach', style: TextStyle(fontSize: 25)),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB (20,0,20,5),
+            child: DropDownButtonTo(),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB (20,5,20,0),
+            child: Button(),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB (20,5,20,0),
+            child: OutputValue(),
+          ),
         ]
       )
     );
   }
 }
 
-class InputWert extends StatelessWidget {
+class InputValue extends StatelessWidget {
+  const InputValue({super.key});
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -66,17 +86,21 @@ class InputWert extends StatelessWidget {
   }
 }
 
-class DropDownButtonVon extends StatefulWidget {
-  State<DropDownButtonVon> createState () => _DropDownButtonVonState();
+class DropDownButtonFrom extends StatefulWidget {
+  const DropDownButtonFrom({super.key});
+
+  @override
+  State<DropDownButtonFrom> createState () => _DropDownButtonFromState();
 }
 
-class _DropDownButtonVonState extends State<DropDownButtonVon> {
+class _DropDownButtonFromState extends State<DropDownButtonFrom> {
   String? value;
 
   @override
   Widget build(BuildContext context) {
     return DropdownButton<String>(
       value: value,
+      isExpanded: true,
       onChanged: (value) => setState(() => this.value = value) ,
       items: items.map(buildMenuItem).toList(),
     );
@@ -85,7 +109,77 @@ class _DropDownButtonVonState extends State<DropDownButtonVon> {
       value: item,
       child: Text(
     item,
-    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
   ),
   );
+}
+
+class DropDownButtonTo extends StatefulWidget {
+  const DropDownButtonTo({super.key});
+
+  @override
+  State<DropDownButtonTo> createState () => _DropDownButtonToState();
+}
+
+class _DropDownButtonToState extends State<DropDownButtonTo> {
+  String? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: value,
+      isExpanded: true,
+      onChanged: (value) => setState(() => this.value = value) ,
+      items: items.map(buildMenuItem).toList(),
+    );
+  }
+  DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+    value: item,
+    child: Text(
+      item,
+      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blue),
+    ),
+  );
+}
+
+class Button extends StatefulWidget {
+  const Button({super.key});
+
+  @override
+  State<Button> createState () => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(100,40),
+        textStyle: const TextStyle(fontSize: 25),
+        backgroundColor: Colors.white38,
+        foregroundColor: Colors.blueAccent,
+      ),
+      child: const Text('Konvertieren'),
+      onPressed: () => print('test'),
+    );
+  }
+}
+
+class OutputValue extends StatelessWidget {
+  const OutputValue({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      enabled: false,
+      decoration: const InputDecoration(
+        border:InputBorder.none,
+        hintText: 'hier kommt der output',
+        labelStyle: TextStyle(
+          fontSize: 30,
+        ),
+      ),
+    );
+  }
 }
